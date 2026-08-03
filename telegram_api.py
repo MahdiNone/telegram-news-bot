8741d067ec63bcaf60adb3e18ffcb5e01a1862be
MahdiNone <slbyaqayy@gmail.com>
2026-08-03 22:39:40 +0330

    Update telegram_api.py

--- telegram_api.py
+++ telegram_api.py
@@ -1,24 +1,43 @@
 import requests
+
 from config import *
 
 def send(text):
 
     requests.post(
+
         f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+
         data={
+
             "chat_id":CHANNEL_ID,
+
             "text":text,
+
             "parse_mode":"HTML"
-        }
+
+        },
+
+        timeout=20
+
     )
 
 def send_photo(photo,caption):
 
     requests.post(
+
         f"https://api.telegram.org/bot{BOT_TOKEN}/sendPhoto",
+
         data={
+
             "chat_id":CHANNEL_ID,
+
             "photo":photo,
+
             "caption":caption
-        }
+
+        },
+
+        timeout=20
+
     )
