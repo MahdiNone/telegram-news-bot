486eec068bfe0d7e83fa1bc14dfc9859da6f2bda
MahdiNone <slbyaqayy@gmail.com>
2026-08-03 22:27:48 +0330

    Create feeds.py

---
+++ feeds.py
@@ -0,0 +1,17 @@
+RSS_FEEDS = [
+
+# ایران
+"https://www.mehrnews.com/rss",
+"https://www.isna.ir/rss",
+"https://www.tabnak.ir/fa/rss/allnews",
+
+# فناوری
+"https://www.zoomit.ir/feed/",
+
+# کریپتو
+"https://cointelegraph.com/rss",
+"https://www.coindesk.com/arc/outboundfeeds/rss/",
+
+# جهان
+"https://feeds.bbci.co.uk/news/rss.xml"
+]
