ce8f34b81df724b8041682ba66fd76965c62fde4
MahdiNone <slbyaqayy@gmail.com>
2026-08-03 22:37:51 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,13 @@
 import os
+from feeds import RSS_FEEDS
 
 BOT_TOKEN=os.getenv("BOT_TOKEN")
 CHANNEL_ID=os.getenv("CHANNEL_ID")
 GEMINI_KEY=os.getenv("GEMINI_KEY")
 
-from feeds import RSS_FEEDS
+MAX_POSTS=8
+REQUEST_TIMEOUT=20
+
+HEADERS={
+"User-Agent":"Mozilla/5.0"
+}
