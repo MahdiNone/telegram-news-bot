c0819d8ce3d3507bf5d145146351c06fd1e43212
MahdiNone <slbyaqayy@gmail.com>
2026-08-03 22:37:19 +0330

    Update feeds.py

--- feeds.py
+++ feeds.py
@@ -4,14 +4,21 @@ RSS_FEEDS = [
 "https://www.mehrnews.com/rss",
 "https://www.isna.ir/rss",
 "https://www.tabnak.ir/fa/rss/allnews",
+"https://www.yjc.ir/fa/rss/allnews",
+
+# اقتصاد
+"https://www.eghtesadonline.com/fa/rss/allnews",
 
 # فناوری
 "https://www.zoomit.ir/feed/",
 
-# کریپتو
+# ارز دیجیتال
 "https://cointelegraph.com/rss",
 "https://www.coindesk.com/arc/outboundfeeds/rss/",
+"https://cryptoslate.com/feed/",
 
 # جهان
-"https://feeds.bbci.co.uk/news/rss.xml"
+"https://feeds.bbci.co.uk/news/rss.xml",
+"https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml"
+
 ]
