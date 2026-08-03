4f6d79127a8515a99fda3b7f03f2b11831733404
MahdiNone <slbyaqayy@gmail.com>
2026-08-03 22:16:17 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+BOT_TOKEN=os.getenv("BOT_TOKEN")
+CHANNEL_ID=os.getenv("CHANNEL_ID")
+GEMINI_KEY=os.getenv("GEMINI_KEY")
+
+RSS_FEEDS=[
+"https://feeds.bbci.co.uk/news/rss.xml",
+"https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml",
+"https://www.theverge.com/rss/index.xml"
+]
