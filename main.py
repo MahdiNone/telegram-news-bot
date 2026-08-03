ce0afe8bbacb3fed2052d7aa3f94eb4b22725a07
MahdiNone <slbyaqayy@gmail.com>
2026-08-03 22:04:34 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print ('hello')
+import os
+import requests
+
+token = os.environ["BOT_TOKEN"]
+channel = os.environ["CHANNEL_ID"]
+
+url = f"https://api.telegram.org/bot{token}/sendMessage"
+
+requests.post(url, data={
+    "chat_id": channel,
+    "text": "✅ سلام! ربات با موفقیت از GitHub Actions اجرا شد."
+})
