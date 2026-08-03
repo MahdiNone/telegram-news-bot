2abe123a895ba4ef5d956620b5e785fd28ded26f
MahdiNone <slbyaqayy@gmail.com>
2026-08-03 22:40:35 +0330

    Update gemini_api.py

--- gemini_api.py
+++ gemini_api.py
@@ -1,17 +1,29 @@
 from google import genai
+
 from config import GEMINI_KEY
 
-client=genai.Client(api_key=GEMINI_KEY)
+client=genai.Client(
+    api_key=GEMINI_KEY
+)
 
-def rewrite(text):
+PROMPT=open(
+    "prompt.txt",
+    encoding="utf8"
+).read()
 
-    prompt=open("prompt.txt",encoding="utf8").read()
+def rewrite(news):
 
-    prompt=prompt.replace("{news}",text)
-
-    r=client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=prompt
+    prompt=PROMPT.replace(
+        "{news}",
+        news
     )
 
-    return r.text
+    r=client.models.generate_content(
+
+        model="gemini-2.5-flash",
+
+        contents=prompt
+
+    )
+
+    return r.text.strip()
